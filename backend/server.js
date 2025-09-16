e80aabe02ac17ddf149cb568421f679f9b92ee89
AbdeYett <58290738+Abderyett@users.noreply.github.com>
2025-09-16 11:10:53 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const upload = multer({
 if (!process.env.EMAIL_USER || !(process.env.EMAIL_PASS || process.env.EMAIL_PASSWORD))
 	throw new Error('EMAIL_USER and EMAIL_PASS (or EMAIL_PASSWORD) must be set in .env');
 
-const createTransporter = nodemailer.createTransporter({
+const createTransporter = nodemailer.createTransport({
 	host  : 'smtp.gmail.com',
 	port  : 465,
 	secure: true,
